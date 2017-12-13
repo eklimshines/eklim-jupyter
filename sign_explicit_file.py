477d1c73b7728f6383e9e820d6a032b6180fac65
Julian Glover <julian.glover@etranssystems.com>
2017-12-13 11:50:07 -0500

    Added sign_explicit_file.py file that accepts a file name as input

---
+++ sign_explicit_file.py
@@ -0,0 +1,17 @@
+from pseudosign import *
+from optparse import OptionParser
+
+parser=OptionParser()
+parser.add_option("-f", "--file", dest="file_to_open", help="file containing data to sign", metavar="FILE")
+parser.add_option("-d", "--data", dest="data", help="to be signed data", metavar="DATA")
+parser.add_option("-p", "--privatekey", dest="privKey", help="private key", metavar="PRIV")
+parser.add_option("-c", "--certificate", dest="certificate", help="certificate to use", metavar="CERT")
+
+
+(options,args)=parser.parse_args()
+with open(options.file_to_open, 'rb') as data_file:
+    file_contents = data_file.read()
+(R, s, digest,cert_dgst) = PseudonymSign(file_contents, options.privKey, options.certificate)
+print(R.output(compress=True, Ieee1609Dot2=True))
+print (Hex(s, radix_256))
+print(cert_dgst[-16:])
